7ef1e19394a8629c644dc4cd95928a8ee1fb8750
wushukai <wushukai@masget.com>
2021-11-29 16:44:39 +0800

    ri

--- src/main/java/L115.java
+++ src/main/java/L115.java
@@ -42,4 +42,50 @@ public class L115 {
 	 * s 和 t 由英文字母组成
 	 */
 
+	/**
+	 *  '' b a b g b a g
+	 *'' 1 1 1 1 1 1 1 1
+	 * b 0 1 1 2 2 3 3 3
+	 * a 0 0 1 1 1 1 4 4
+	 * g 0 0 0 0 1 1 1 5
+	 * dp[i][j] 表示字符串S的前j个元素中有多少个 T的前i个元素
+	 * dp[i][j] = dp[i][j - 1] + (T[i - 1] == S[j - 1] ? dp[i - 1][j - 1] : 0);
+	 * 计算dp[i][j]时要判断S[j-1]==T[i-1],切勿搞错
+	 * @param s
+	 * @param t
+	 * @return
+	 */
+	public static int handle(String s, String t) {
+		int l1 = s.length();
+		int l2 = t.length();
+
+		if (l1 == 0 || l2 == 0) {
+			return 0;
+		}
+		if (l2 > l1) {
+			return 0;
+		}
+		int[][] dp = new int[l2 + 1][l1 + 1];
+		for (int i = 0; i < l1 + 1; i++) {
+			dp[0][i] = 1;
+		}
+		for (int i = 1; i < l2 + 1; i++) {
+			char ci = t.charAt(i - 1);
+			for (int j = 1; j < l1 + 1; j++) {
+				char cj = s.charAt(j - 1);
+				dp[i][j] = dp[i][j - 1] + (ci == cj ? dp[i - 1][j - 1] : 0);
+			}
+		}
+
+		return dp[l2][l1];
+	}
+
+	public static void main(String[] args) {
+		String s;
+		String t;
+		s = "babgbag";
+		t = "bag";
+		System.err.println(handle(s, t));
+	}
+
 }
