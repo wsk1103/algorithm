dfb90c5558a0ce1d986d0b9d2beebd3bfc8a9100
wushukai <wushukai@masget.com>
2021-12-17 10:41:33 +0800

    min

--- src/main/java/L128.java
+++ src/main/java/L128.java
@@ -1,3 +1,7 @@
+import com.alibaba.fastjson.JSON;
+
+import java.util.*;
+
 /**
  * @author sk
  * @time 2021/12/17
@@ -29,5 +33,61 @@ public class L128 {
      *
      * 进阶：可以设计并实现时间复杂度为 O(n) 的解决方案吗？
      */
+
+    public static int handle(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        Set<Integer> vis = new HashSet<>();
+        int max = 0;
+        for (int num : nums) {
+            if (vis.contains(num)) {
+                continue;
+            }
+            int tempNum = num;
+            int temp = 0;
+            while (set.contains(tempNum)) {
+                if (vis.contains(tempNum)) {
+                    break;
+                }
+                vis.add(tempNum);
+                temp++;
+                tempNum++;
+            }
+            tempNum = num - 1;
+            while (set.contains(tempNum)) {
+                if (vis.contains(tempNum)) {
+                    break;
+                }
+                vis.add(tempNum);
+                temp++;
+                tempNum--;
+            }
+            max = Math.max(max, temp);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] n;
+        n = new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        System.err.println(handle(n));
+        List<Integer> set = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < 1000; i++) {
+            int tt = r.nextInt(1000);
+            if (set.contains(tt)) {
+                continue;
+            }
+            set.add(tt);
+        }
+        n = new int[set.size()];
+        for (int i = 0; i < set.size(); i++) {
+            n[i] = set.get(i);
+        }
+        System.err.println(JSON.toJSONString(n));
+        System.err.println(handle(n));
+    }
 
 }
