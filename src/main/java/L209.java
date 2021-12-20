b2f4691badb1e7476a3d338c8e0a4e8d1dc20d25
wushukai <wushukai@masget.com>
2021-12-20 14:51:14 +0800

    min

--- src/main/java/L209.java
+++ src/main/java/L209.java
@@ -5,7 +5,7 @@
  **/
 public class L209 {
 
-    /**
+    /*
      * 给定一个含有 n 个正整数的数组和一个正整数 target 。
      * <p>
      * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
@@ -81,8 +81,6 @@ public class L209 {
                 if (nums[left] >= target) {
                     return 1;
                 }
-//                right++;
-//                continue;
             }
             if (sum < target) {
                 right++;
