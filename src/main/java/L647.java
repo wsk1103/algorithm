31a4b4271c7152bea685356690d902bcf59f87d4
wushukai <wushukai@masget.com>
2021-12-23 15:05:27 +0800

    min

--- src/main/java/L647.java
+++ src/main/java/L647.java
@@ -10,7 +10,7 @@ import java.util.Map;
  **/
 public class L647 {
 
-    /**
+    /*
      * 给定一个字符串 s ，请计算这个字符串中有多少个回文子字符串。
      *
      * 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
@@ -85,21 +85,40 @@ public class L647 {
 
     //TODO 2.中心拓展
     public static int handle2(String s) {
-        return 0;
+        int to = 0;
+        for (int i = 0; i < s.length(); i++) {
+            to = loop2(s, i, i, to);
+            to = loop2(s, i, i + 1, to);
+        }
+        return to;
+    }
+
+    private static int loop2(String s, int left, int right, int to) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            to++;
+        }
+        return to;
     }
     public static void main(String[] args) {
         String s;
         s = "aabccba";
         System.err.println(handle(s));
+        System.err.println(handle2(s));
         s = "aaa";
         System.err.println(handle(s));
+        System.err.println(handle2(s));
         s = "abc";
         System.err.println(handle(s));
+        System.err.println(handle2(s));
         s = "aba";
         System.err.println(handle(s));
+        System.err.println(handle2(s));
 
         s = "abccbaabccbaabccbaabccbaabccba";
         System.err.println(handle(s));
+        System.err.println(handle2(s));
     }
 
 }
